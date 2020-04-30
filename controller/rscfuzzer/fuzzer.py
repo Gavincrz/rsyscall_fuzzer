7f963bbc7557f89f06dda4cd123ca8a8a3ec8dd3
Gavin <gavin.cui@mail.utoronto.ca>
2020-04-30 14:42:22 -0400

    pront all syscall list

--- controller/rscfuzzer/fuzzer.py
+++ controller/rscfuzzer/fuzzer.py
@@ -513,6 +513,9 @@ class Fuzzer:
             syscall_orders.append(self.parse_syscall_order())
         self.compare_syscall_orders(syscall_orders, 'v')
         self.check_new_syscall(syscall_orders)
+        self.print_differ(syscall_orders[0], 0, f'differ/{self.target_name}_allv_0')
+        self.print_differ(syscall_orders[1], 0, f'differ/{self.target_name}_allv_1')
+        self.print_differ(syscall_orders[1], 0, f'differ/{self.target_name}_allv_2')
 
         syscall_orders = []
         for client in self.target.get("clients"):
@@ -524,6 +527,9 @@ class Fuzzer:
                 syscall_orders.append(self.parse_syscall_order(False))
             self.compare_syscall_orders(syscall_orders, 'c')
             self.check_new_syscall(syscall_orders)
+            self.print_differ(syscall_orders[0], 0, f'differ/{self.target_name}_allv_0')
+            self.print_differ(syscall_orders[1], 0, f'differ/{self.target_name}_allv_1')
+            self.print_differ(syscall_orders[1], 0, f'differ/{self.target_name}_allv_2')
 
 
         pass
